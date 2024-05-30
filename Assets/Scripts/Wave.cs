bfb896bf2e023b44b32571cdf873ae316bfe7893
RedLoftwing <MLoftwing@gmail.com>
2024-05-30 01:44:08 +0100

    Actual progress on compute shader!

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -18,15 +18,12 @@ public class Wave : MonoBehaviour
     public ComputeShader computeShader;
     private ComputeBuffer _vertexBuffer;
     private int _kernelHandle;
-    private VertexData[] vertexData;
-    public MyVertices[] vertexData2;
+    public MyVertices[] vertexData;
 
     public struct VertexData
     {
         public uint vertexID;
-        public Vector3 vertexStartPosition;
         public Vector3 vertexPosition;
-        public Vector3 vertexWorldPosition;
     }
     
     void Awake()
@@ -55,19 +52,20 @@ public class Wave : MonoBehaviour
             // };
         }
 
-        vertexData2 = new MyVertices[_waterMeshVertices.Length];
+        vertexData = new MyVertices[_waterMeshVertices.Length];
         for (int i = 0; i < _waterMeshVertices.Length; i++)
         {
-            vertexData2[i] = new MyVertices(i , _waterMeshVertices[i]);
+            vertexData[i] = new MyVertices(i , _waterMeshVertices[i]);
         }
         
-        _vertexBuffer = new ComputeBuffer(vertexData2.Length, sizeof(uint) + (sizeof(float) * 3) + (sizeof(float)));
+        //_vertexBuffer = new ComputeBuffer(vertexData2.Length, sizeof(uint) + (sizeof(float) * 3) + (sizeof(float) * 3) + (sizeof(float) * 3));
+        _vertexBuffer = new ComputeBuffer(vertexData.Length, sizeof(uint) + (sizeof(float) * 3));
     }
 
     private void Update()
     {
         FindLocalVertices();
-        UseComputeShader2();
+        UseComputeShader();
     }
 
     private void FindLocalVertices()
@@ -84,21 +82,12 @@ public class Wave : MonoBehaviour
         Vector3 minBounds = transform.position - halfSize;
         Vector3 maxBounds = transform.position + halfSize;
 
-        for (int i = 0; i < vertexData2.Length; i++)
+        for (int i = 0; i < vertexData.Length; i++)
         {
-            //Vector3 worldVertex = _waterMeshFilter.transform.TransformPoint(allVertices[i]); // Convert to world space
-            // if (IsWithinBounds(vertexData[i].vertexWorldPosition, minBounds, maxBounds))
-            // {
-            //     _localVertices.Add(new MyVertices(i, vertexData[i].vertexWorldPosition));
-            //     //_localVertices1.Add();
-            //     //localVertexIndices.Add(i);
-            // }
-
-            Vector3 worldVertex = _waterMeshFilter.transform.TransformPoint(vertexData2[i].GetVector());
+            Vector3 worldVertex = _waterMeshFilter.transform.TransformPoint(vertexData[i].GetVector());
             if (IsWithinBounds(worldVertex, minBounds, maxBounds))
             {
-                //Debug.Log("The vertex to add: " + vertexData2[i].GetIndex());
-                _localVertices.Add(vertexData2[i]);
+                _localVertices.Add(vertexData[i]);
             }
         }
     }
@@ -109,91 +98,45 @@ public class Wave : MonoBehaviour
                 point.y >= minBounds.y && point.y <= maxBounds.y &&
                 point.z >= minBounds.z && point.z <= maxBounds.z);
     }
-
-    private void UseComputeShader0()
-    {
-        // // Debugging: Log the count of _localVertices
-        if (_localVertices == null || _localVertices.Count == 0)
-        {
-            Debug.LogWarning("No vertices found. Skipping ComputeBuffer creation.");
-        }
-        
-        ////IF the distance between this gameobject and the first main node in the array is less than 1...destroy this gameobject.
-        //if (Vector3.Distance(transform.position, nodeArray[1].position) < 1)
-        //{
-        //    Destroy(this.gameObject);
-        //}
-        
-        //Add vertices to the data.
-        VertexData[] vertexData = new VertexData[_localVertices.Count];
-        for (int i = 0; i < _localVertices.Count; i++)
-        {
-            vertexData[i] = new VertexData
-            {
-                vertexID = (uint)_localVertices[i].vertexIndex,
-                vertexPosition = _localVertices[i].vertexVector
-            };
-            // if (vertexData[i].vertexID != _localVertices[i].vertexIndex)
-            // {
-            //     Debug.LogError("vertexIndex: " + _localVertices[i].vertexIndex + ". vertexPosition: " + _localVertices[i].vertexVector);
-            // }
-        }
-        
-        //
-        _vertexBuffer = new ComputeBuffer(vertexData.Length, sizeof(uint) + sizeof(float) * 3);
-        _vertexBuffer.SetData(vertexData);
-        computeShader.SetBuffer(_kernelHandle, "localVerticesBuffer", _vertexBuffer);
-        int threadGroups = Mathf.CeilToInt(vertexData.Length / 8.0f);
-        computeShader.Dispatch(_kernelHandle, threadGroups, threadGroups, 1);
-        _vertexBuffer.GetData(vertexData);
-        
-        Debug.Log(vertexData.Length + " " + _waterMeshVertices.Length);
-        for (int i = 0; i < vertexData.Length; i++)
-        {
-            _waterMeshVertices[vertexData[i].vertexID] = vertexData[i].vertexPosition;
-        }
-        
-        //
-        _vertexBuffer.Release();
-        
-        _waterMesh.vertices = _waterMeshVertices;
-        _waterMesh.RecalculateBounds();
-    }
-
-    private void UseComputeShader1()
-    {
-        VertexData[] localVertexArray = new VertexData[_localVertices.Count];
-        for (int i = 0; i < _localVertices.Count; i++)
-        {
-            localVertexArray[i] = vertexData[i];
-        }
-        _vertexBuffer.SetData(localVertexArray);
-    }
     
-    private void UseComputeShader2()
+    private void UseComputeShader()
     {
         VertexData[] arrayBuffer = new VertexData[_localVertices.Count];
         for (int i = 0; i < _localVertices.Count; i++)
         {
             arrayBuffer[i].vertexID = (uint)_localVertices[i].vertexIndex;
             arrayBuffer[i].vertexPosition = _localVertices[i].vertexVector;
-            arrayBuffer[i].vertexStartPosition = _localVertices[i].vertexVector;
-            arrayBuffer[i].vertexWorldPosition = _waterMeshFilter.transform.TransformPoint(_localVertices[i].GetVector());
+            if (i == 0)
+            {
+                Debug.Log("POS BEFORE COMPUTE: " + arrayBuffer[i].vertexPosition);
+            }
+            //arrayBuffer[i].vertexStartPosition = _localVertices[i].vertexVector;
+            //arrayBuffer[i].vertexWorldPosition = _waterMeshFilter.transform.TransformPoint(_localVertices[i].GetVector());
         }
         _vertexBuffer.SetData(arrayBuffer);
         computeShader.SetBuffer(_kernelHandle, "localVerticesBuffer", _vertexBuffer);
-        int threadGroups = Mathf.CeilToInt(vertexData2.Length / 8.0f);
+        int threadGroups = Mathf.CeilToInt(vertexData.Length / 8.0f);
         computeShader.Dispatch(_kernelHandle, threadGroups, threadGroups, 1);
         
         //
         _vertexBuffer.GetData(arrayBuffer);
         Vector3[] vertices = new Vector3[_waterMeshVertices.Length];
-        for (int i = 0; i < _localVertices.Count; i++)
+
+        // for (int i = 0; i < _waterMeshVertices.Length; i++)
+        // {
+        //     vertices[i] = vertexData[i].GetVector();
+        // }
+        for (int i = 0; i < arrayBuffer.Length; i++)
         {
+            if (i == 0)
+            {
+                Debug.Log("POS AFTER COMPUTE: " + arrayBuffer[i].vertexPosition);
+            }
+        
             vertices[i] = arrayBuffer[i].vertexPosition;
         }
 
-        _waterMeshVertices = vertices;
+        _waterMesh.vertices = vertices;
         _waterMesh.RecalculateNormals();
         _waterMeshFilter.mesh = _waterMesh;
     }
